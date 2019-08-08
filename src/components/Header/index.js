6c51f14c9a3fc7d6c302cb41b724603e6b8216c7
AdiKub <adiletkub@gmail.com>
2019-08-08 13:45:22 +0600

    finished header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import './styles.scss'
+import Logo from '../../assets/images/logo.svg'
 
 
 const Header = () => {
-
+    const navs = ['resume', 'about me', 'contacts', 'works']
     return (
         <div className='header'>
-            Header
+            <div className='container'>
+                <div className='header-title'>
+                    <div className='header-title-logo'>
+                        <img
+                            className='header-title-logo__img'
+                            src={Logo}
+                            alt='logo'/>
+                    </div>
+                    <h1 className='header-title__text'>
+                        ADILET KUBATBEK UULU
+                    </h1>
+                </div>
+
+                    <ul className='header-navs'>
+                        {navs.map(nav =>
+                            <li
+                                className='header-navs__link'
+                                key={nav}>
+                                {nav}
+                            </li> )}
+                    </ul>
+
+            </div>
         </div>
     )
 }
