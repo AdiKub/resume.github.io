78c1aebe2e42c0fe84f1a236bcec4bcbf981aacd
adilet <adilet.kubatbekov@sibers.com>
2020-05-19 19:24:24 +0600

    added build as docs

---
+++ docs/precache-manifest.76497b3c973153599571fed8fd7ee0a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16136c4b3f29f567e52fdb5b5d1c7aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "466f9904dad0f58675e4",
+    "url": "/static/css/main.dc3c5c6d.chunk.css"
+  },
+  {
+    "revision": "9911535134c54a3c369d",
+    "url": "/static/js/2.7738189e.chunk.js"
+  },
+  {
+    "revision": "466f9904dad0f58675e4",
+    "url": "/static/js/main.3a30247f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
