4b3d917c56e39f3b192e660c0205ee08a99a9f14
adilet <adilet.kubatbekov@sibers.com>
2020-05-19 20:33:46 +0600

    rename build to docs

---
+++ docs/precache-manifest.fac1199e526676556beb948ff20e8f10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4394b40761b6a0eda724f6efcd58ec0",
+    "url": "/adiletkub.com/index.html"
+  },
+  {
+    "revision": "8ce97ece22746b505ea1",
+    "url": "/adiletkub.com/static/css/main.dc3c5c6d.chunk.css"
+  },
+  {
+    "revision": "9911535134c54a3c369d",
+    "url": "/adiletkub.com/static/js/2.7738189e.chunk.js"
+  },
+  {
+    "revision": "8ce97ece22746b505ea1",
+    "url": "/adiletkub.com/static/js/main.93bf17a0.chunk.js"
+  },
+  {
+    "revision": "6e6e3b75c8154ad22e72",
+    "url": "/adiletkub.com/static/js/runtime~main.e68947e8.js"
+  }
+]);
